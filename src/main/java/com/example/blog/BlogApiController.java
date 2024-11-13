426c42cbf27cb64ad8467d4a31c24fe773c77485
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-11-13 23:47:30 +0900

    feat: Add endpoint to retrieve all articles in BlogApiController

--- src/main/java/com/example/blog/BlogApiController.java
+++ src/main/java/com/example/blog/BlogApiController.java
@@ -2,12 +2,16 @@ package com.example.blog;
 
 import com.example.blog.domain.Article;
 import com.example.blog.dto.AddArticleRequest;
+import com.example.blog.dto.ArticleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,5 +23,12 @@ public class BlogApiController {
         Article savedArticle = blogService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll().stream().map(ArticleResponse::new).toList();
+
+        return ResponseEntity.ok().body(articles);
     }
 }
