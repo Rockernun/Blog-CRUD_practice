76075e6c944c21d0db25e3cff4ffcb7bf69eb177
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-11-18 12:26:33 +0900

    feat: add API endpoint to get article by ID
    - Added GET endpoint '/api/articles/{id}' to retrieve an article by its ID
    
    - Returns ArticleResponse wrapped in a ResponseEntity

--- src/main/java/com/example/blog/BlogApiController.java
+++ src/main/java/com/example/blog/BlogApiController.java
@@ -6,10 +6,7 @@ import com.example.blog.dto.ArticleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +27,12 @@ public class BlogApiController {
         List<ArticleResponse> articles = blogService.findAll().stream().map(ArticleResponse::new).toList();
 
         return ResponseEntity.ok().body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok().body(new ArticleResponse(article));
     }
 }
